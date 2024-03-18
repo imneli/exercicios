5ec6a7b395faffbac2e949e69a0f3601bc2f5974
Matheus Rivera <contatodoneli@gmail.com>
2024-03-18 11:26:22 -0300

    exercicios de repetição de palavras e numeros

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #exercicio 1 - poligonos
 
-'''lados = int(input("Diga o numero de lados: "))
+lados = int(input("Diga o numero de lados: "))
 if lados < 3:
     print("não é um poligono")
 elif lados > 5:
@@ -14,13 +14,13 @@ else:
         forma = 'pentagono'
     medida = int(input("Qual a medida do lado?"))
     perimetro = lados*medida
-    print(f"O poligono é um {forma} com perimetro de {perimetro}")'''
+    print(f"O poligono é um {forma} com perimetro de {perimetro}")
 
 # exercicio 2 - verificar impar e par
 
 #sem while (ruim) 
 
-'''n1 = int(input("1 | Digite um numero: "))
+n1 = int(input("1 | Digite um numero: "))
 n2 = int(input("2 | Digite um numero: "))
 n3 = int(input("3 | Digite um numero: "))
 n4 = int(input("4 | Digite um numero: "))
@@ -59,7 +59,7 @@ while rep < 5: # se n informar < 5 n vai parar nunca, só vai repetir pq vai con
         pares += 1
     rep = rep + 1
 impares = 5 - pares
-print(f"O numero de pares foi {pares} e o de impares foi {impares}")'''
+print(f"O numero de pares foi {pares} e o de impares foi {impares}")
 
 # pedir senha até acertar
 
@@ -69,7 +69,76 @@ while senha != 1234:
     senha = int(input("Digite uma senha: "))
 print("senha correta.")
 
-#teste
+#exercicio senha teste
+
+tentativa = 0
+senha = int(input("Digite uma senha: "))
+while senha != 1234 and tentativa < 2:
+    print('Senha incorreta.')
+    senha = int(input("Digite uma senha: "))
+    tentativa += 1
+if senha == 1234:
+    print("senha correta.")  
+else:
+    print("senha incorreta")                                                                                                        
+
+#pedir 10 numeros e somar todos
+
+qt_num = 0
+soma = 0
+while qt_num < 10:
+    num = int(input("Digite um numero: "))
+    qt_num += 1
+    soma += num
+print(f"A soma dos valores é {soma}")
+
+#calcular 10 numeros
+
+qt_num = 0
+i = 1
+while qt_num < 10:
+    num = int(input("Digite um numero: "))
+    qt_num += 1
+    i *= num
+print(f"A soma dos valores é {i}")
+
+'''qt_num = 0
+i = 1
+while qt_num < 10:
+    num = int(input("Digite um numero: "))
+    qt_num += 1
+    i *= num
+print(f"A soma dos valores é {i}")'''
+
+i = 0
+soma = 0
+while i < 10:
+    i += 1
+    soma += num
+print(f"A soma dos valores é {soma}")
+
+#somar 10 numeros sem input
+
+i = 11
+soma = 0
+while i > 0:
+    i -= 1
+    soma += i
+    print(soma)
+
+#palavras
+
+i = 0
+frase = ''
+while i < 10:
+    pal = input("Digite uma palavra: ")
+    i += 1
+    frase += pal
+    print(frase)
+
+
+
+
 
 
 
